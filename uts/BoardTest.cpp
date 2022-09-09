1d0b0362430fe3687a8e6bd703ec4d8441d4e042
Mateusz <mateuszluczkowski@gmail.com>
2022-09-09 20:00:59 +0200

    implemented test for getField

--- uts/BoardTest.cpp
+++ uts/BoardTest.cpp
@@ -7,15 +7,25 @@ protected:
     utils::Board board3x3{ 3,3 };
     utils::Board board4x6{ 4,6 };
 };
+
 TEST_F(BoardTest, getSizeShouldReturn3X3WhenInitValueIs3x3) {
     std::pair<int, int> actualSize = board3x3.getSize();
     ASSERT_EQ(actualSize.first, 3);
     ASSERT_EQ(actualSize.second, 3);
 }
+
 TEST_F(BoardTest, getSizeShouldReturn4X6WhenInitValueIs4x6) {
     std::pair<int, int> actualSize = board4x6.getSize();
     EXPECT_EQ(actualSize.first, 4);
     EXPECT_EQ(actualSize.second, 6);
+}
+
+TEST_F(BoardTest, getFieldIn3x3BoardShouldReturnEmptyForAllCoord) {
+    for (int row{ 0 }; row < 3; row++) {
+        for (int column{ 0 }; column < 3; column++) {
+            EXPECT_EQ(board3x3.getField(row, column), EMPTY);
+        }
+    }
 }
 TEST(BoardTesting, getSizeShouldReturn0X0WhenNumOfRowsIsLessThan3) {
     utils::Board board2x3{ 2,3 };
