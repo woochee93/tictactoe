091fa8b812e14382eb4be27ff4fbc635217d110e
Mateusz <mateuszluczkowski@gmail.com>
2022-09-09 18:47:21 +0200

    implemented names for init grid loop

--- src/Board.cpp
+++ src/Board.cpp
@@ -8,9 +8,11 @@ std::pair<int, int> utils::Board::getSize() {
 }
 
 void utils::Board::initGrid() {
-   for (int row{ 0 }; row < size.first;row++) {
+   int numOfRows = size.first;
+   int numOfColumns = size.second;
+   for (int row{ 0 }; row < numOfRows;row++) {
       grid.push_back(std::vector<Field>{});
-      for (int column = 0; column < size.second; column++) {
+      for (int column = 0; column < numOfColumns; column++) {
          grid.at(row).push_back(EMPTY);
       }
    }
