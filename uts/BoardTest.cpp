bcf32f38d6077f1acd6fb6a729aee72b0b6cfbb0
Mateusz <mateuszluczkowski@gmail.com>
2022-09-09 18:57:23 +0200

    clean up boardTest

--- uts/BoardTest.cpp
+++ uts/BoardTest.cpp
@@ -6,9 +6,10 @@ class BoardTest :public::testing::Test {
 protected:
     utils::Board board;
 };
-TEST_F(BoardTest, checkInitSize) {
-   ASSERT_EQ(board.getSize().first, 3);
-   ASSERT_EQ(board.getSize().second, 3);
+TEST_F(BoardTest, checkConstructorWithOutPassedArg) {
+    std::pair<int,int> actualSize = board.getSize();
+   ASSERT_EQ(actualSize.first, 3);
+   ASSERT_EQ(actualSize.second, 3);
 }
 TEST_F(BoardTest, checkGridSize){
    
